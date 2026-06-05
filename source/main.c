5e337b45e4fececa3ebf11ae2ff7e7be247dff15
Cyrilange <cyril.r.a.salamite@gmail.com>
2026-06-05 11:50:58 +0200

    last test

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: csalamit <csalamit@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 11:33:04 by csalamit          #+#    #+#             */
-/*   Updated: 2026/06/04 17:50:19 by csalamit         ###   ########.fr       */
+/*   Updated: 2026/06/05 11:50:40 by csalamit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	launch_md5(t_flags flag, char *str, int fd, unsigned char *buf, size
 
 	if (buf) {
 		ft_md5_algo(buf, buf_len, digest);
-		print_flag(flag, "MD5", digest, 16, (char *)buf, -1);
+		print_flag(flag, "MD5", digest, 16, str, -1);
 		return ;
 	}
 	if (fd >= 0) {
@@ -28,8 +28,7 @@ static void	launch_md5(t_flags flag, char *str, int fd, unsigned char *buf, size
 		print_flag(flag, "MD5", digest, 16, str, 1);
 		if (content)
 			free(content);
-	}
-	else {
+	} else {
 		ft_md5_algo((unsigned char *)str, ft_strlen(str), digest);
 		print_flag(flag, "MD5", digest, 16, str, 0);
 	}
@@ -41,8 +40,8 @@ static void	launch_sha256(t_flags flag, char *str, int fd, unsigned char *buf, s
 	size_t			len;
 
 	if (buf) {
-		ft_sha256_algo(buf, buf_len , digest);
-		print_flag(flag, "SHA256", digest, 32, (char *)buf, -1);
+		ft_sha256_algo(buf, buf_len, digest);
+		print_flag(flag, "SHA256", digest, 32, str, -1);
 		return ;
 	}
 	if (fd >= 0) {
@@ -51,8 +50,7 @@ static void	launch_sha256(t_flags flag, char *str, int fd, unsigned char *buf, s
 		print_flag(flag, "SHA256", digest, 32, str, 1);
 		if (content)
 			free(content);
-	}
-	else {
+	} else {
 		ft_sha256_algo((unsigned char *)str, ft_strlen(str), digest);
 		print_flag(flag, "SHA256", digest, 32, str, 0);
 	}
@@ -80,80 +78,79 @@ static void	close_file(int fd) {
 		close(fd);
 }
 
-int main(int argc, char **argv) {
-    char    *commands[] = {"md5", "sha256", NULL};
-    void    (*hash_functions[])(t_flags, char *, int, unsigned char *, size_t) = {launch_md5, launch_sha256};
-    t_flags flags = {0};
-    int     cmd_idx = -1;
-    int     arg_idx = 2;
-    int     stdin_processed = 0;
+int	main(int argc, char **argv) {
+	char	*commands[] = {"md5", "sha256", NULL};
+	void	(*hash_functions[])(t_flags, char *, int, unsigned char *, size_t) = {launch_md5, launch_sha256};
+	t_flags	flags = {0};
+	int		cmd_idx = -1;
+	int		arg_idx = 2;
 
-    if (argc < 2) {
-        ft_error("usage: ft_ssl command [flags] [file/string]\n");
-        return (1);
-    }
-    for (int i = 0; commands[i]; i++) {
-        if (ft_strcmp(argv[1], commands[i]) == 0) {
-            cmd_idx = i;
-            break ;
-        }
-    }
-    if (cmd_idx == -1) {
-        ft_error("ft_ssl: Error: '");
-        ft_error(argv[1]);
-        ft_error("' is an invalid command.\n");
-        return (1);
-    }
-    while (arg_idx < argc && argv[arg_idx][0] == '-') {
-        int j = 1;
-        while (argv[arg_idx][j]) {
-            if (argv[arg_idx][j] == 'p') flags.p = 1;
-            else if (argv[arg_idx][j] == 'q') flags.q = 1;
-            else if (argv[arg_idx][j] == 'r') flags.r = 1;
-            else if (argv[arg_idx][j] == 's') {
-                flags.s = 1;
-                j++;
-                break ;
-            }
-            else { ft_error("unknown option\n"); return (1);}
-            j++;
-        }
-        arg_idx++;
-    }
-	if (flags.p)
-	{
-		unsigned char *content;
-		size_t len;
-	
+	if (argc < 2) {
+		ft_error("usage: ft_ssl command [flags] [file/string]\n");
+		return (1);
+	}
+	for (int i = 0; commands[i]; i++) {
+		if (ft_strcmp(argv[1], commands[i]) == 0) {
+			cmd_idx = i;
+			break ;
+		}
+	}
+	if (cmd_idx == -1) {
+		ft_error("ft_ssl: Error: '");
+		ft_error(argv[1]);
+		ft_error("' is an invalid command.\n");
+		return (1);
+	}
+	while (arg_idx < argc && argv[arg_idx][0] == '-') {
+		int j = 1;
+		while (argv[arg_idx][j]) {
+			if (argv[arg_idx][j] == 'p') flags.p = 1;
+			else if (argv[arg_idx][j] == 'q') flags.q = 1;
+			else if (argv[arg_idx][j] == 'r') flags.r = 1;
+			else if (argv[arg_idx][j] == 's') {
+				flags.s = 1;
+				j++;
+				break ;
+			} else {
+				ft_error("unknown option\n");
+				return (1);
+			}
+			j++;
+		}
+		arg_idx++;
+	}
+	if (flags.p) {
+		unsigned char	*content;
+		size_t			len;
+
 		content = read_fd(0, &len);
-		if (content)
-		{
-			if (len > 0 && content[len - 1] == '\n') {content[len - 1] = '\0'; len--;}
+		if (content) {
+			if (len > 0 && content[len - 1] == '\n') {
+				content[len - 1] = '\0';
+				len--;
+			}
 			hash_functions[cmd_idx](flags, (char *)content, -1, content, len);
 			free(content);
 		}
-		flags.p = 0;
 	}
-    if (flags.s) {
-        if (arg_idx >= argc) {
-            ft_error("ft_ssl: option requires an argument -- s\n");
-            return (1);
-        }
-        hash_functions[cmd_idx](flags, argv[arg_idx], -1, NULL, 0);
-        arg_idx++;
-    }
-    if (arg_idx < argc) {
-        while (arg_idx < argc) {
-            int fd = open_file(argv[arg_idx]);
-            if (fd >= 0) {
-                hash_functions[cmd_idx](flags, argv[arg_idx], fd, NULL, 0);
-                close_file(fd);
-            }
-            arg_idx++;
-        }
-    }
-    else if (!flags.s && !stdin_processed && arg_idx == 2) {
-        hash_functions[cmd_idx](flags, NULL, 0, NULL, 0);
-    }
-    return (0);
+	if (flags.s) {
+		if (arg_idx >= argc) {
+			ft_error("ft_ssl: option requires an argument -- s\n");
+			return (1);
+		}
+		hash_functions[cmd_idx](flags, argv[arg_idx], -1, NULL, 0);
+		arg_idx++;
+	}
+	if (arg_idx < argc) {
+		while (arg_idx < argc) {
+			int fd = open_file(argv[arg_idx]);
+			if (fd >= 0) {
+				hash_functions[cmd_idx](flags, argv[arg_idx], fd, NULL, 0);
+				close_file(fd);
+			}
+			arg_idx++;
+		}
+	} else if (!flags.s && !flags.p)
+		hash_functions[cmd_idx](flags, NULL, 0, NULL, 0);
+	return (0);
 }
--- source/print_algo.c
+++ source/print_algo.c
@@ -6,84 +6,65 @@
 /*   By: csalamit <csalamit@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 10:08:07 by csalamit          #+#    #+#             */
-/*   Updated: 2026/06/04 18:00:20 by csalamit         ###   ########.fr       */
+/*   Updated: 2026/06/05 11:49:15 by csalamit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_ssl.h"
 
-static void	print_digest(unsigned char *d, int len)
-{
+static void	print_digest(unsigned char *d, int len) {
 	int	i = 0;
-
-	while (i < len)
-	{
+	while (i < len) {
 		ft_printf_hex(d[i]);
 		i++;
 	}
 }
 
-static void	print_name_algo(char *algo, char *str, int is_file, int len)
-{
-	write(1, algo, ft_strlen(algo));
-	write(1, " (", 2);
-	if (str)
-	{
-		if (is_file)
+void	print_flag(t_flags f, char *algo, unsigned char *d, int len, char *str, int is_file) {
+	if (is_file == -1) {
+		write(1, "(\"", 2);
+		if (str)
 			write(1, str, ft_strlen(str));
-		else
-			write(1, str, len);
-	}
-	write(1, ") = ", 4);
-}
-
-static void	print_string_only(char *str, int is_file, int len)
-{
-	if (!str)
-		return ;
-
-	if (is_file)
-		write(1, str, ft_strlen(str));
-	else
-		write(1, str, len);
-}
-
-static void	print_stdin(char *str)
-{
-
-	write(1, "(", 1);
-	if (str)
-		write(1, str, ft_strlen(str));
-	write(1, ") = ", 4);
-}
-
-void	print_flag(t_flags f, char *algo, unsigned char *d, int len, char *str, int is_file)
-{
-	if (f.q)
-	{
+		write(1, "\")= ", 4);
 		print_digest(d, len);
 		write(1, "\n", 1);
 		return ;
 	}
-
-	if (f.r)
-	{
+	if (f.q) {
+		print_digest(d, len);
+		write(1, "\n", 1);
+		return ;
+	}
+	if (f.r) {
 		print_digest(d, len);
 		write(1, " ", 1);
-		print_string_only(str, is_file, len);
+		if (str) {
+			if (is_file == 1)
+				write(1, str, ft_strlen(str));
+			else {
+				write(1, "\"", 1);
+				write(1, str, ft_strlen(str));
+				write(1, "\"", 1);
+			}
+		}
 		write(1, "\n", 1);
 		return ;
 	}
-
-	if (is_file == -1)
-	{
-		print_stdin(str);
-		print_digest(d, len);
-		write(1, "\n", 1);
-		return ;
+	if (is_file == 1) {
+		write(1, algo, ft_strlen(algo));
+		if (str) {
+			write(1, " (", 2);
+			write(1, str, ft_strlen(str));
+			write(1, ") = ", 4);
+		} else
+			write(1, "(stdin)= ", 9);
+	} else if (is_file == 0) {
+		write(1, algo, ft_strlen(algo));
+		write(1, " (\"", 3);
+		if (str)
+			write(1, str, ft_strlen(str));
+		write(1, "\") = ", 5);
 	}
-
-	print_name_algo(algo, str, is_file, len);
 	print_digest(d, len);
 	write(1, "\n", 1);
 }
